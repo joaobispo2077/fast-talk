5b85412bc0aac433e78ed8ee14413b3ebf1a14fd
João Bispo <joaobispo2077@gmail.com>
2022-04-15 16:26:21 -0300

    feat: setup websockets server

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,18 @@
 import express from "express";
+import path from "path";
+import { createServer } from "http";
+import { Server as SocketIoServer } from 'socket.io';
 
 const APP_PORT = 3000;
 const app = express();
 
-app.listen(APP_PORT, () => console.log(`Server is running on port ${APP_PORT}`));
+const server = createServer(app);
+app.use(express.static(path.join(__dirname, "..", "public")));
+
+const io = new SocketIoServer(server);
+io.on("connection", (socket) => {
+  console.log("New client connected");
+  console.log("socket id:", socket.id);
+});
+
+server.listen(APP_PORT, () => console.log(`Server is running on port ${APP_PORT}`));
