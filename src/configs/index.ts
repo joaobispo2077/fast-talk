0e8e927b9283c263f3a29b121da0532a4fd3f0d0
João Bispo <joaobispo2077@gmail.com>
2022-04-17 20:43:44 -0300

    feat: add heroku environment PORT

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -1,5 +1,5 @@
 import 'dotenv/config';
 
-export const APP_PORT = process.env.APP_PORT || 3000;
+export const APP_PORT = process.env.PORT || process.env.APP_PORT || 3000;
 export const DATABASE_URL =
   process.env.DATABASE_URL || 'mongodb://localhost/chat-app';
