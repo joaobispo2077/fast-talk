a2e4e763de1534ffc8c262450b141962b7b5a455
João Bispo <joaobispo2077@gmail.com>
2022-04-22 14:36:45 -0300

    feat: add form validation logic with reactHookForm

--- mobile/src/screens/JoinChat/index.tsx
+++ mobile/src/screens/JoinChat/index.tsx
@@ -1,3 +1,4 @@
+import { yupResolver } from '@hookform/resolvers/yup';
 import {
   Box,
   Button,
@@ -7,11 +8,44 @@ import {
   Stack,
   Text,
 } from 'native-base';
-import React from 'react';
+import React, { useState } from 'react';
+import { Controller, useForm } from 'react-hook-form';
+import * as yup from 'yup';
 
 import { ICONS } from '@src/components/icons';
 
+type JoinChatFormData = {
+  chatname: string;
+  username: string;
+  expirationInDays: number;
+};
+
+const schema = yup.object().shape({
+  chatname: yup.string().required('The chat name is required'),
+  username: yup.string().required('The username is required'),
+  expirationInDays: yup
+    .number()
+    .typeError('The value must be a number')
+    .positive('The value must be a positive number')
+    .required('The expiration in days is required'),
+});
+
 export const JoinChat = () => {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<JoinChatFormData>({
+    resolver: yupResolver(schema),
+  });
+  const hasFormError = Object.keys(errors).length > 0;
+
+  const onSubmit = async (data: JoinChatFormData) => {
+    console.log('submiting with ', data);
+  };
+
+  console.log('errors', errors);
+
   return (
     <Box
       backgroundColor={'gray.200'}
@@ -33,27 +67,74 @@ export const JoinChat = () => {
         <Text fontSize="xl" color="gray.400">
           Connect with your friends and talk until the chat expires.
         </Text>
-        <FormControl>
+        <FormControl isRequired isInvalid={hasFormError}>
           <Stack space="2.5" marginTop={8}>
-            <Input
-              size="2xl"
-              placeholder="Chat name"
-              InputRightElement={ICONS.chat}
+            <Controller
+              control={control}
+              render={({ field: { onChange, onBlur, value } }) => (
+                <Input
+                  size="2xl"
+                  placeholder="Chat ame"
+                  InputRightElement={ICONS.chat}
+                  value={value}
+                  onChangeText={(val) => onChange(val)}
+                  onBlur={onBlur}
+                />
+              )}
+              name="chatname"
+              rules={{ required: 'Chatname is required', minLength: 3 }}
+              defaultValue=""
             />
-            <Input
-              size="2xl"
-              placeholder="Username"
-              InputRightElement={ICONS.user}
+            <FormControl.ErrorMessage>
+              {errors.chatname?.message}
+            </FormControl.ErrorMessage>
+            <Controller
+              control={control}
+              render={({ field: { onChange, onBlur, value } }) => (
+                <Input
+                  size="2xl"
+                  placeholder="Username"
+                  InputRightElement={ICONS.user}
+                  value={value}
+                  onChangeText={(val) => onChange(val)}
+                  onBlur={onBlur}
+                />
+              )}
+              name="username"
+              rules={{ required: 'Username is required', minLength: 4 }}
             />
-            <Input
-              size="2xl"
-              placeholder="Expiration in days"
-              type={'number'}
-              InputRightElement={ICONS.calendar}
+            <FormControl.ErrorMessage>
+              {errors.username?.message}
+            </FormControl.ErrorMessage>
+            <Controller
+              control={control}
+              render={({ field: { onChange, onBlur, value } }) => (
+                <Input
+                  size="2xl"
+                  placeholder="Expiration in days"
+                  keyboardType="numeric"
+                  InputRightElement={ICONS.calendar}
+                  value={String(value)}
+                  onChangeText={(val) => onChange(val)}
+                  onBlur={onBlur}
+                  defaultValue="1"
+                />
+              )}
+              name="expirationInDays"
+              rules={{ required: 'expirationInDays is required' }}
             />
+            <FormControl.ErrorMessage>
+              {errors.expirationInDays?.message}
+            </FormControl.ErrorMessage>
           </Stack>
         </FormControl>
-        <Button marginTop={4}>Join to the chat</Button>
+        <Button
+          marginTop={4}
+          onPress={handleSubmit(onSubmit)}
+          colorScheme="amber"
+        >
+          Join to the chat
+        </Button>
       </Box>
     </Box>
   );
